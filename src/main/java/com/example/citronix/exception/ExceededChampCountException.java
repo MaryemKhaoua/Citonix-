662e95c0223c3c110acdda017c8851814ba3ab37
Maryem khaoua <maryem.khaoua@gmail.com>
2024-11-21 15:59:41 +0100

    crud for champ

---
+++ src/main/java/com/example/citronix/exception/ExceededChampCountException.java
@@ -0,0 +1,7 @@
+package com.example.citronix.exception;
+
+public class ExceededChampCountException extends RuntimeException {
+    public ExceededChampCountException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/example/citronix/exception/InvalidChampAreaException.java
@@ -0,0 +1,7 @@
+package com.example.citronix.exception;
+
+public class InvalidChampAreaException extends RuntimeException {
+    public InvalidChampAreaException(String message) {
+        super(message);
+    }
+}
